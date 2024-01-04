fbbf4d156bfcfdbc1958dcc30755cca9ff38a7cd
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-04 17:15:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function fetchCharacterInfo() {
     const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d";
-    const characterName = encodeURIComponent(document.getElementById("top-searchbar").value);
-
-    // List of servers to iterate over
     const servers = ["연", "무휼", "세류", "해명", "낙랑", "하백", "비류", "온조"];
 
     servers.forEach(serverName => {
-        const url = `https://open.api.nexon.com/baramy/v1/id?character_name=${characterName}&server_name=${encodeURIComponent(serverName)}`;
+        const characterName = encodeURIComponent(document.getElementById("top-searchbar").value);
+        const url = `https://open.api.nexon.com/baramy/v1/id?character_name=${characterName}&server_name=${serverName}`;
 
         fetch(url, {
             headers: {
@@ -44,3 +42,7 @@ function fetchCharacterInfo() {
         });
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchCharacterInfo();
+});
