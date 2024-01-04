9495105715f3a4cf411895a220701f531bbb8ed5
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-04 18:21:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Function to fetch character info for a given server
     const fetchCharacterInfo = async (server) => {
-        const characterName = encodeURIComponent(document.getElementById("characterName").value);
-        const apiKey = 'test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d';  // Replace with your actual API key
+        const characterInput = document.getElementById("characterName");
+        const serverInput = document.getElementById("serverName");
+
+        // Check if the required inputs are available
+        if (!characterInput || !serverInput) {
+            console.error("Required inputs not found.");
+            return;
+        }
+
+        const characterName = encodeURIComponent(characterInput.value);
+        const apiKey = 'test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d';
         const apiUrl = `https://open.api.nexon.com/baramy/v1/id?character_name=${characterName}&server_name=${server}&apikey=${apiKey}`;
 
         try {
