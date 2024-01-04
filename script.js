f0eeac6f422edd91f15189bea48b50aa9a3614c7
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-04 16:59:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function fetchCharacterInfo() {
     const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d";
     const characterName = encodeURIComponent(document.getElementById("top-searchbar").value);
-
-    // List of servers to iterate over
     const servers = ["연", "무휼", "세류", "해명", "낙랑", "하백", "비류", "온조"];
 
-    // Iterate over servers
     servers.forEach(serverName => {
         const url = `https://open.api.nexon.com/baramy/v1/id?character_name=${characterName}&server_name=${encodeURIComponent(serverName)}`;
 
@@ -16,13 +13,13 @@ function fetchCharacterInfo() {
             })
             .then(response => response.json())
             .then(data => {
+                console.log(`Server: ${serverName}`, data); // Log the response data to the console
+
                 const resultDiv = document.getElementById(`${serverName.toLowerCase()}Resp`);
 
                 if (data.error) {
-                    // No information found for this server
                     resultDiv.textContent = `정보를 찾을 수 없습니다.`;
                 } else {
-                    // Display ocid for this server
                     resultDiv.textContent = `ocid(${serverName}): ${data.ocid}`;
                 }
             })
