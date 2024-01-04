cba8acc4bfc659b40ff8e1b599c3073784d908e5
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-04 21:34:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fetchCharacterInfo() {
     const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d";
     const characterName = encodeURIComponent(document.getElementById("characterName").value);
-    //const serverName = encodeURIComponent(document.getElementById("serverName").value);
+
     const servers = ["연", "무휼", "세류", "해명", "낙랑", "하백", "비류", "온조"];
 
     servers.forEach(serverName => {
