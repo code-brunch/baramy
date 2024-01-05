e5297554fe3858c5c06e8cb91e01533ec63f4d26
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-05 12:01:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
             characterNameInput.value = '캐릭터명 또는 길드';
         }
     });
+
+    // 검색 버튼 클릭 이벤트 처리
+    document.getElementById('searchButton').addEventListener('click', function () {
+        const isInputEmpty = characterNameInput.value.trim() === '';
+
+        if (isInputEmpty) {
+            alert('캐릭터명을 입력 후 검색해주세요.');
+            characterNameInput.focus(); // 검색창에 다시 포커스를 맞춥니다.
+        } else {
+            fetchCharacterInfo();
+        }
+    });
 });
 
 async function fetchCharacterInfo() {
     const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d";
     const characterNameInput = document.getElementById("characterName");
     const characterName = encodeURIComponent(characterNameInput.value.trim()); // trim을 사용하여 공백 제거
-
-    if (!characterName) {
-        alert("캐릭터명을 입력 후 검색해주세요.");
-        return;
-    }
 
     const servers = ["연", "무휼", "세류", "해명", "낙랑", "하백", "비류", "온조"];
     const resultDiv = document.getElementById("result");
