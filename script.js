147f61935cc389c5545eb793b7d4cd3a9fdbef1a
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-08 10:23:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -238,6 +238,7 @@ async function fetchCharacterInfo() {
                                     break;
                                 default:
                                     break;
+                        }
                     }
                 });
             }
