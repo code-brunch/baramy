36be71e5394a2a6a6d7c11b9939bd9acebf8bee7
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-09 11:34:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -298,7 +298,8 @@ async function fetchCharacterInfo() {
                     // 이미지가 없으면 새로운 img 태그를 생성
                     if (!castleImage) {
                         castleImage = document.createElement('img');
-                        document.querySelector('.sub12-upper').appendChild(castleImage);
+                        //document.querySelector('.sub12-upper').appendChild(castleImage);
+                        document.querySelector('.sub12-upper').insertBefore(castleImage, document.querySelector('.sub12-title'));
                     }
                     castleImage.style.width = '25px'; // 25px로 수정 (원래는 30px로 되어 있었음)
                     castleImage.style.height = '25px'; // 25px로 수정 (원래는 30px로 되어 있었음)
