09af19a172e3d59ea05c16f478dd8f3eee67786a
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-09 15:16:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 포커스 이벤트 처리
     characterNameInput.addEventListener('focus', function () {
-        if (characterNameInput.value === '캐릭터명 또는 길드') {
+        if (characterNameInput.value === '수행자명을 입력해주세요.') {
             characterNameInput.value = '';
         }
     });
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 포커스 아웃 이벤트 처리
     characterNameInput.addEventListener('blur', function () {
         if (characterNameInput.value === '') {
-            characterNameInput.value = '캐릭터명 또는 길드';
+            characterNameInput.value = '수행자명을 입력해주세요.';
         }
     });
 });
