230b1445b317951e9a999bbee03cc0a923529880
codebrunch <sjunkyle77@gmail.com>
2024-01-09 23:22:44 +0900

    const modfied4

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const characterNameInput = document.getElementById('characterName');
-
-    // 포커스 이벤트 처리
-    characterNameInput.addEventListener('focus', function () {
+if (typeof document !== 'undefined') {
+    document.addEventListener('DOMContentLoaded', function () {
+      const characterNameInput = document.getElementById('characterName');
+  
+      // 포커스 이벤트 처리
+      characterNameInput.addEventListener('focus', function () {
         if (characterNameInput.value === '수행자명을 입력해주세요.') {
-            characterNameInput.value = '';
+          characterNameInput.value = '';
         }
-    });
-
-    // 포커스 아웃 이벤트 처리
-    characterNameInput.addEventListener('blur', function () {
+      });
+  
+      // 포커스 아웃 이벤트 처리
+      characterNameInput.addEventListener('blur', function () {
         if (characterNameInput.value === '') {
-            characterNameInput.value = '수행자명을 입력해주세요.';
+          characterNameInput.value = '수행자명을 입력해주세요.';
         }
+      });
     });
-});
+  }
+  
 
 async function fetchCharacterInfo() {    
     const apiKey = API_KEY;
