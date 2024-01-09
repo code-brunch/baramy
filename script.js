91f1d0840eb88fef88a9227911a22919d19d6464
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-09 13:24:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 async function fetchCharacterInfo() {    
-    const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa08ec93cbb06b86feee55ebcbed5a6401040fc9f0162a1fec40ac4b8e45e56924d";
+    const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa0fc195af0fccda90f8413fc1752215176c98f6cd472d4226608ef1a241f84aa9c";
     const characterNameInput = document.getElementById("characterName");
     const characterName = encodeURIComponent(characterNameInput.value.trim()); // trim을 사용하여 공백 제거
     
