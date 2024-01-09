2169714fd8ce70232891315160a5eaf08ffc9510
codebrunch <sjunkyle77@gmail.com>
2024-01-09 15:46:34 +0900

    test sthng wrong

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const API_KEY = config.apikey;
-
 document.addEventListener('DOMContentLoaded', function () {
     const characterNameInput = document.getElementById('characterName');
 
@@ -19,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 async function fetchCharacterInfo() {    
-    const apiKey = ${API_KEY};
+    const apiKey = "test_ad6c0a6934215fad4b75dfc81d40caa0fc195af0fccda90f8413fc1752215176c98f6cd472d4226608ef1a241f84aa9c";
     const characterNameInput = document.getElementById("characterName");
     const characterName = encodeURIComponent(characterNameInput.value.trim()); // trim을 사용하여 공백 제거
     
