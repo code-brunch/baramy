c72a248028b8e1aaf7010b33d8001e3f0383b211
codebrunch <sjunkyle77@gmail.com>
2024-01-09 21:37:17 +0900

    yml and js modified 1

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 async function fetchCharacterInfo() {    
-    const apiKey = "API_KEY_PLACEHOLDER";
+    const apiKey = "{{API_KEY}}";
     const characterNameInput = document.getElementById("characterName");
     const characterName = encodeURIComponent(characterNameInput.value.trim()); // trim을 사용하여 공백 제거
     
