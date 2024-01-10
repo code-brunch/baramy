a8f1bfd38bba174b5b9f7e341178efdfc36a04c4
codebrunch <sjunkyle77@gmail.com>
2024-01-10 22:08:08 +0900

    const modfied0110v40

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const characterNameInput = document.getElementById('characterName');
+
+    // 포커스 이벤트 처리
+    characterNameInput.addEventListener('focus', function () {
+        if (characterNameInput.value === '수행자명을 입력해주세요.') {
+            characterNameInput.value = '';
+        }
+    });
+
+    // 포커스 아웃 이벤트 처리
+    characterNameInput.addEventListener('blur', function () {
+        if (characterNameInput.value === '') {
+            characterNameInput.value = '수행자명을 입력해주세요.';
+        }
+    });
+
     // WASM 모듈을 비동기적으로 로드
     const wasmModule = (async () => {
         const response = await fetch('./main.wasm');
