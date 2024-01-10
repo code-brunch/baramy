4ca67a3a44272f98985bca682c3f4f43fe1d9068
codebrunch <sjunkyle77@gmail.com>
2024-01-10 21:51:49 +0900

    const modfied0110v34

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
 async function fetchCharacterInfo(apiKey) {
     const characterNameInput = document.getElementById("characterName");
     const characterName = encodeURIComponent(characterNameInput.value.trim()); // trim을 사용하여 공백 제거
