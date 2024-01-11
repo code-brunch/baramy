d905079bf504774eed29d0656c8c455d1a3634ab
codebrunch <sjunkyle77@gmail.com>
2024-01-11 22:48:40 +0900

    baramy beta v1.0 release upload r9

--- Assets/npik/config.js
+++
@@ -1,7 +0,0 @@
-export const preconfig = {
-    value : 'ad6c0a6934215fad4b75dfc81d40caa0fc195af0fcc'
-};
-
-export const postconfig = {
-    value : 'da90f8413fc1752215176c98f6cd472d4226608ef1a241f84aa9c'
-};
